89c5ca6d0f995fa1999ce98eceee9db25105608f
FauzyBOT <fauzycbn@gmail.com>
2022-08-02 22:08:30 +0700

    PulsaGratis

---
+++ Pulsa.sh
@@ -0,0 +1,109 @@
+#!/system/xbin/bash
+#Bersihkan Layar
+clear
+blue='\e[1;34m'
+green='\e[1;32m'
+purple='\[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+sleep 0
+echo "\033[32;1m=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*"
+toilet -f standard -F gay "PULSAGRATIS"
+echo "\033[31;1m=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*"
+echo ""
+echo "Author : " Fauzi / Realz"
+" Team : " FAUZI CYBER TEAM"
+echo " Kontak Me : " 085791677204"
+" Pesan : "Jangan Di Salah Gunakan Dan Disebar :) Karena Nanti Dosa Tanggung Sendiri:)"
+sleep 0
+echo "ThanksTo:"
+echo "Allmember FAUZI CYBER TEAM"
+echo "\033[32;1m=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*"
+echo ""
+sleep 0
+echo "Kumpulan Kumpulan Pulsa All Operator"
+echo "Gunakan Pakai +62 #Contoh +62xxx:"
+read p;
+
+echo "Nomor Sedang Di Proses"
+sleep 0
+echo "\033[37;1m10%" 
+sleep 0
+echo "20%" 
+sleep 0
+echo "30%" 
+sleep 0
+echo "40%" 
+sleep 0
+echo "50%" 
+sleep 0
+echo "60%" 
+sleep 0
+echo "\033[31;1mNomor Gagal Di Proses..""\033[32;1mSilahkan Masukkan Kembali No Anda:"
+read p;
+sleep 0
+echo "Sedang Menuju Proses..."
+sleep 0
+echo "\033[37;1m10%"
+sleep 0
+echo "20%"
+sleep 0
+echo "30%"
+sleep 0
+echo "40%"
+sleep 0
+echo "50%"
+sleep 0
+echo "60%"
+sleep 0
+echo "70%"
+sleep 0
+echo "80%"
+sleep 0
+echo "90%"
+sleep 0
+echo "100%"
+sleep 0
+echo "\033[32;1mSilahkan Masukkan Jumlah Yang Anda Mau:"
+read p;
+sleep 0
+echo "Sedang Menuju Proses..."
+echo "\033[37;1m10%"
+sleep 0
+echo "20%"
+sleep 0
+echo "30%"
+sleep 0
+echo "40%"
+sleep 0
+echo "50%"
+sleep 0
+echo "60%"
+sleep 0
+echo "70%"
+sleep 0
+echo "80%"
+sleep 0
+echo "90%"
+sleep 0
+echo "100%"
+sleep 0
+echo "\033[34;1mJumlah Yang Anda Masukkan Sukses...""\033[32;1mMasuk Silahkan Di Cek:v"
+sleep 0
+sleep 0
+echo "Ketik Exit Untuk Keluar"
+read p;
+sleep 0
+sleep 0
+sleep 0
+
+echo "Pesan:Jika Script Ini Tidak Berfungsi Pada Anda Berarti Ada Kesalahan Dalam Script Ini"
+sleep 0
+echo "TerimaKasih Untuk Yang Memakai Script Ini"
+sleep 0
+echo "ThanksTo:MrUncle - Fauzi - Owl Cyber Team"
+sleep 0
+sleep 0
+exit
